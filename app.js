c2328f60dcfc84a60db61dba6e53b589cacd84c1
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-05-26 14:44:54 -0400

    dfasdfsa

--- app.js
+++ app.js
@@ -3,4 +3,14 @@ fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json')
     .then(data => console.log(data))
     .catch(error => console.log(error))
 
-    console.log('new update');
+ $.ajax({
+    url: "https://data.cityofnewyork.us/resource/erm2-nwe9.json",
+    type: "GET",
+    data: {
+      "$limit" : 5000,
+      "$$app_token" : "YOURAPPTOKENHERE"
+    }
+}).then(function(data) {
+  alert("Retrieved " + data.length + " records from the dataset!");
+  console.log(data);
+});
