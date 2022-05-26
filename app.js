538cec876e60cd92f0d9ce495a9981d1b8083d4e
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-05-26 14:54:04 -0400

    got the data

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json')
-    .then(response => response(json()))
+    .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.log(error))
 
- $.ajax({
-    url: "https://data.cityofnewyork.us/resource/erm2-nwe9.json",
-    type: "GET",
-    data: {
-      "$limit" : 5000,
-      "$$app_token" : "YOURAPPTOKENHERE"
-    }
-}).then(function(data) {
-  alert("Retrieved " + data.length + " records from the dataset!");
-  console.log(data);
-});
+//  $.ajax({
+//     url: "https://data.cityofnewyork.us/resource/erm2-nwe9.json",
+//     type: "GET",
+//     data: {
+//       "$limit" : 5000,
+//       "$$app_token" : "YOURAPPTOKENHERE"
+//     }
+// }).then(function(data) {
+//   alert("Retrieved " + data.length + " records from the dataset!");
+//   console.log(data);
+// });
