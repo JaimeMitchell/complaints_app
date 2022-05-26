083091721cbbf5b9be15676368f7fdea5b5df1b8
wajjih <wajjih22@gmail.com>
2022-05-26 14:08:23 -0400

    new name

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json')
     .then(response => response(json()))
     .then(data => console.log(data))
     .catch(error => console.log(error))
+
+    console.log('new update');
