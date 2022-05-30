cde52b9d5a51729a396430915fdc5ffae2140d3c
AinurPro <aakhmetcheyeva@gmail.com>
2022-05-30 18:49:46 -0400

    updates in js,updates in CSS

--- app.js
+++ app.js
@@ -1,36 +1,62 @@
-// let limit = 10
-// const borough = BROOKLYN
-//function
-function borough(borough, limit) {
-    return fetch('https://data.cityofnewyork.us/resource/erm2-nwe9.json?borough=' + borough + "&$limit=" + limit)
+//1. function fetches the data and has 2 parameters, the borough and the limit, the limit variable will be a callback function, num, which takes the user text input as a number of complaints they want to see. This is attached the the button functions below that represent the 5 boroughs of NYC. This function also contains the expressions that print the number of complaints for the borough
+
+function borough(districtName, limit) {
+
+    fetch(`https://data.cityofnewyork.us/resource/erm2-nwe9.json?agency=NYPD&borough=${districtName}&$limit=${limit}&$order=complaint_type`)
         .then(response => response.json())
-        .then(data => data)
+        .then(data => {
+            data.map((complaint, index) => {
+                let div = document.createElement('div')
+                let innerDiv = document.createElement('div')
+                let p1 = document.createElement('p')
+                let p2 = document.createElement('p')
+                let btn = document.createElement('button')
+                p1.textContent = complaint.complaint_type
+                innerDiv.appendChild(p1)
+                innerDiv.appendChild(btn)
+                div.appendChild(innerDiv)
+                div.appendChild(p2)
+                const display = document.getElementById('display')
+                display.appendChild(div)
+                p2.style.visibility = 'hidden'
+                btn.textContent = "Police Response?"
+                btn.classList.add('button')
+                innerDiv.classList.add('inner-div')
+                // div.classList.add('wrapper')
+                // Nested button that shows more details by changing the p2 text from hidden to visible.
+                btn.addEventListener('click', function () {
+                    p2.style.visibility = 'visible'
+                    p2.textContent = complaint.resolution_description
+                })
+                console.log(data.sort())
+            })
+        })
         .catch(error => console.log(error))
-
 }
-//1. Create a new variable called 'display' and assign it the div with the id of 'display'
-function num () {
+
+//2. Create function to store user input number which is then called as a variable for borough function above. This is hoisting so may be better practice to put it above the higher order 'borough' function that calls it.
+
+function getNumByInput() {
     const textBox = document.querySelector("#textBox")
-    return textBox.value}
-    // clears out the input value prop
-    // input.value = ''
-    const display = document.querySelector('.display')
-    display.textContent = textBox.value
-//2. Create Functions for buttons(num, manhattan, brooklyn, bronx, queens, statenIsland) 
-//update the value of the div variable using textContent
-// const Manhattan = () => result.textContent
+    let inputValue = textBox.value || 10
+    console.log('bla bla ',inputValue)
+    return inputValue
+}
 
-const manhattan = () => borough("MANHATTAN", num())
+//3. Create Functions for buttons(num, manhattan, brooklyn, bronx, queens, statenIsland) 
 
-const brooklyn = () => borough("BROOKLYN", num())
+const manhattan = () => borough("MANHATTAN", getNumByInput())
 
-const bronx = () => borough("BRONX", num())
+const brooklyn = () => borough("BROOKLYN", getNumByInput())
 
-const queens = () => borough("QUEENS", num())
+const bronx = () => borough("BRONX", getNumByInput())
 
-const statenIsland = () => borough("STATEN ISLAND", num())
+const queens = () => borough("QUEENS", getNumByInput())
 
-//3. Attach Functions to Buttons, this is the connection between the buttons and the 'display' div
+const statenIsland = () => borough("STATEN ISLAND", getNumByInput())
+
+//4. Attach Functions to Buttons, this is the connection between the buttons and the 'display' div
+
 const ManhattanBtn = document.querySelector('#manhattan')
 ManhattanBtn.addEventListener('click', manhattan)
 
@@ -45,3 +71,22 @@ QueensBtn.addEventListener('click', queens)
 
 const StatenIslandBtn = document.querySelector('#statenIsland')
 StatenIslandBtn.addEventListener('click', statenIsland)
+
+//5. MAP
+document.getElementById('map')
+var map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([37.41, 8.82]),
+      zoom: 4
+    })
+    
+  })
+// Things to try for bonus:
+// make_point
+// $order=:id
